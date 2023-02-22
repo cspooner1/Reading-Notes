efda6b9213e1e7c3ee586c99fd3697edcc1d86bd
cspooner1 <christopherspooner423@gmail.com>
2023-02-22 15:26:56 -0600

    update

---
+++ LABS/Class 2 Lab/index.js
@@ -0,0 +1,13 @@
+let age ="";
+let school ="";
+let color ="";
+let job ="";
+let cook ="";
+
+function displayage() {
+    age = prompt("Am I 19 or not?");
+    age.toUpperCase();
+    if (age !=y);
+    alert ("Your actually smart!");
+    document.getElementById("age").innerHTML += age
+}
